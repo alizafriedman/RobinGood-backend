5da0de7588b2791acff94f70b2dc0ce1df544035
alizafriedman <alizafriedman123@gmail.com>
2020-06-18 12:16:54 -0400

    auth0 inf basic setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-# import json
-# from six.moves.urllib.request import urlopen
-# from functools import wraps
+import json
+from six.moves.urllib.request import urlopen
+from functools import wraps
 from flask import Flask, request, jsonify, _request_ctx_stack
 from flask_cors import cross_origin, CORS
 from .routes import home, rd
@@ -20,4 +20,48 @@ app.register_blueprint(home.bp)
 
 
 rd.init_app(app)
+
+# Error handler
+
+  @app.errorhandler(AuthError)
+   def handle_auth_error(ex):
+        response = jsonify(ex.error)
+        response.status_code = ex.status_code
+        return response
+
+    # This doesn't need authentication
+
+    @app.route("/api/public")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    def public():
+        response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+        return jsonify(message=response)
+
+    # This needs authentication
+
+    @app.route("/api/private")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    @requires_auth
+    def private():
+        token = request.headers.get('Authorization')
+        print(token)
+        response = "Hello from a private endpoint! You need to be authenticated to see this."
+        return jsonify(message=response)
+
+    # This needs authorization
+
+    @app.route("/api/private-scoped")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    @requires_auth
+    def private_scoped():
+        if requires_scope("read:messages"):
+            response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+            return jsonify(message=response)
+        raise AuthError({
+            "code": "Unauthorized",
+            "description": "You don't have access to this resource"
+        }, 403)
+
+    return app
+
 
--- documentation/backendRoutes.md
+++ documentation/backendRoutes.md
@@ -2,24 +2,24 @@
 
 1. Users:
    - POST "/users"
-     - This endpoint will create a new user
+     -  endpoint will create a new user
    - PUT "/users/:user_id"
-     - This endpoint will update user information
+     -  endpoint will update user information
 
 
 2. Charities:
     - GET "/"
-        - This endpoint returns home page with featured charities
+        - endpoint returns home page with featured charities
     - POST@requires_auth "/charity/:user_id"
-        - This endpoint saves a new charity donated to for a user
+        - endpoint saves a new charity donated to for a user
     - PUT@requires_auth "/charity/:user_id"
-        - This endpoint updates a users portfolio of charity donated to
+        - endpoint updates a users portfolio of charity donated to
     - DELETE@requires_auth "/charity/:user_id"
-        - This endpoint deletes a charity donated to 
+        - endpoint deletes a charity donated to 
 
 
 3. Portfolio:
-    - GET "/:user_id/portfolio"
+    - GET@requires_auth "/:user_id/portfolio"
         - endpoint returns an overview of a users portfolio
     - DELETE@requires_auth "/:user_id/portfolio"
         - endpoint deletes charities from the portfolio
@@ -27,7 +27,7 @@
 
  4. Favorites:
    - POST@requires_auth "/:charity_id/favorite"
-     - This endpoint adds a favorite to set @id for logged in user
+     - This endpoint adds a favorite to charity @id for logged in user
    - DELETE@requires_auth "/charity:id/favorite/delete
-     - This endpoint removes a favorite to set @id for logged in user
+     - This endpoint removes a favorite to charity @id for logged in user
     
