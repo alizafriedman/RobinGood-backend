8a7c9d4fa21a1bfd76ce6568f69376e50a5628f1
alizafriedman <alizafriedman123@gmail.com>
2020-06-19 23:38:58 -0400

    api info add, json convert

--- app/routes/charity.py
+++ app/routes/charity.py
@@ -17,7 +17,7 @@ def handle_auth_error(ex):
     return response
 
 
-@bp.route('/search')
+@bp.route('')
 def charities():
     body = request.json
     charity_id = body['charity_id']
@@ -26,19 +26,40 @@ def charities():
         return charity
     else:
         token = request.headers.get('Authorization')
-        req = requests.get('http://data.orghunter.com/v1/charitybasic?user_key=3527271551210ee6dbcb09d5e20c8a41&ein=590774235',
+        req = requests.get(f'http://data.orghunter.com/v1/charityfinancial?user_key=3527271551210ee6dbcb09d5e20c8a41&ein={charity_id}',
                             headers={'Authorization': token}).content
         charityInfo = json.loads(req)
         info = charityInfo['data']
-        ein_number = info['ein']
+        charity_id = info['ein']
         name = info['name']
         city = info['city']
-        states = info['state']
-        dictionary = {'name':name, }
-        add = rd.getset('city', city)
-        print(city)
-        test = rd.get(str(add))
-        
-        return str(add)
-        
+        state = info['state']
+        zipCode = info['zipCode']
+        category = info['nteeClass']
+        revenue = info['totrevenue']
+        functionalExpenses = info['totfuncexpns']
+        fundraising = info['grsincfndrsng']
+        contributions = info['totcntrbgfts']
+
+        dictionary = { 
+        'name': name,
+        'city': city,
+        'state': state,
+        'zip code': zipCode,
+        'category': category,
+        'total revenue': int(revenue),
+        'total functional expenses': int(functionalExpenses),
+        'gross fundraising': int(fundraising),
+        'total contributions': int(contributions)
+        }
+
+        data = json.dumps(dictionary)
+        rd.set(charity_id, data)
+        result = rd.get(charity_id)
+        res = json.loads(result)
+        db.session.add(str(charity_id))
+        db.session.commit()
+        print(res)
+        return res
+    
     
